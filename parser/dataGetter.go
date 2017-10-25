120f2cd375dcf8a367f2d4e839402cc390373b3e
VTL-Developer <thach.vincent.le@gmail.com>
2017-10-25 00:06:35 -0400

    Added HTML data conversion from http response. Changed the signature for JSON data converserion from a http request to match HTML function.

--- parser/dataGetter.go
+++ parser/dataGetter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/PuerkitoBio/goquery"
 	"io/ioutil"
 	"net/http"
 	"time"
@@ -9,16 +10,32 @@ import (
 
 var client = http.Client{Timeout: 10 * time.Second}
 
-func getDataAsJson(rsp *http.Response, target interface{}) error {
+func getDataAsJson(rsp *http.Response) (*interface{}, error) {
 	defer rsp.Body.Close()
+	var target interface{}
 	jsonBody, err := ioutil.ReadAll(rsp.Body)
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	err = json.Unmarshal(jsonBody, target)
-	return err
+	err = json.Unmarshal(jsonBody, &target)
+	return &target, err
+}
+
+func getDataAsHtml(rsp *http.Response) (*interface{}, error) {
+	var convertedDocument interface{}
+	var err error
+	defer rsp.Body.Close()
+
+	document, err := goquery.NewDocumentFromResponse(rsp)
+
+	if err != nil {
+		return nil, err
+	}
+
+	convertedDocument = *document
+	return &convertedDocument, nil
 }
 
 func getHttpBodyResponse(url string, headers map[string]string) ([]byte, error) {
--- parser/dataGetter_test.go
+++ parser/dataGetter_test.go
@@ -2,9 +2,73 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"net/http"
+	"net/http/httptest"
+	"path"
 	"testing"
 )
+
+func Test_getDataAsJson_With_PriceHistoryJson(t *testing.T) {
+	var rsp http.Response
+	rsp.Body = &MockClosingBuffer{bytes.NewBufferString(`{"price":"54.05", "datetime":"2017-10-24T22:15:21.000000Z"}`)}
+	jsonBody, err := getDataAsJson(&rsp)
+
+	if err != nil {
+		t.Errorf("Error occured: %v", err)
+		return
+	}
+
+	convertedJsonBody, ok := (*jsonBody).(map[string]interface{})
+
+	if !ok {
+		t.Errorf("Conversion was not successful.\nOriginal: %q\nConverted: %q", jsonBody, convertedJsonBody)
+		return
+	}
+
+	if convertedJsonBody["price"] != "54.05" {
+		t.Errorf("JSON body price is %q, should be %q", convertedJsonBody["price"], "54.05")
+	}
+
+	if convertedJsonBody["datetime"] != "2017-10-24T22:15:21.000000Z" {
+		t.Errorf("JSON body datetime is %q, should be %q", convertedJsonBody["datetime"], "2017-10-24T22:15:21.000000Z")
+	}
+}
+
+func Test_getDataAsHtml(t *testing.T) {
+	var convertedDocument goquery.Document
+	s := httptest.NewServer(http.HandlerFunc(httpHandler))
+	rsp, _ := http.Get(s.URL)
+
+	document, err := getDataAsHtml(rsp)
+
+	if err != nil {
+		t.Errorf("Error occured: %v", err)
+		return
+	}
+
+	convertedDocument, ok := (*document).(goquery.Document)
+
+	if !ok {
+		t.Errorf("Conversion failed.\nOriginal: %q\nConverted: %q", document, convertedDocument)
+		return
+	}
+
+	span := convertedDocument.Find("span")
+
+	if span.Text() != "hello" {
+		t.Errorf("Span text found is %q, should be %q", span.Text(), "hello")
+	}
+
+	if !span.HasClass("check") {
+		t.Error("Span does not have class \"check\"")
+	}
+}
+
+func httpHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(rw, `<html><head></head><body><span class="check">hello</span></body></html>`, path.Base(r.URL.Path))
+}
 
 type MockClosingBuffer struct {
 	*bytes.Buffer
@@ -12,29 +76,4 @@ type MockClosingBuffer struct {
 
 func (mcb *MockClosingBuffer) Close() error {
 	return nil
-}
-
-func Test_getDataAsJson_With_PriceHistory(t *testing.T) {
-	var rsp http.Response
-	var jsonBody interface{}
-	rsp.Body = &MockClosingBuffer{bytes.NewBufferString(`{"price":"54.05", "datetime":"2017-10-24T22:15:21.000000Z"}`)}
-	err := getDataAsJson(&rsp, &jsonBody)
-
-	if err != nil {
-		t.Errorf("Error occured: %v", err)
-	}
-
-	convertedJsonBody, ok := jsonBody.(map[string]interface{})
-
-	if !ok {
-		t.Errorf("Conversion was not successful.\nOriginal: %q\nConverted: %q", jsonBody, convertedJsonBody)
-	}
-
-	if convertedJsonBody["price"] != "54.05" {
-		t.Errorf("JSON body price should is %q, should be %q", convertedJsonBody["price"], "54.05")
-	}
-
-	if convertedJsonBody["datetime"] != "2017-10-24T22:15:21.000000Z" {
-		t.Errorf("JSON body datetime should is %q, should be %q", convertedJsonBody["datetime"], "2017-10-24T22:15:21.000000Z")
-	}
 }
