9300783e8e4f884179c47e4cd223d053436f1022
VTL-Developer <thach.vincent.le@gmail.com>
2017-11-19 12:57:40 -0500

    Added logic to allow multiple goroutines to update multiple items.

--- getter.go
+++ getter.go
@@ -1,7 +1,56 @@
 package main
 
+func GetAndUpdateDataForItems(itemsToUpdate []*ItemData, numOfThreads int) []*ItemData {
+	var nextItem *ItemData = nil
+
+	if numOfThreads < 0 {
+		panic("Number of threads should be 1 or greater")
+	}
+
+	idChan := make(chan int)
+	jobChans := startThreads(itemsToUpdate, numOfThreads, idChan)
+	numOfJobs := len(jobChans)
+	leftover := itemsToUpdate[min(numOfJobs, len(itemsToUpdate)):]
+
+	var threadId int
+	logDebug("Have %v active thread(s) running", numOfJobs)
+	for numOfJobs > 0 {
+		threadId = <-idChan
+		nextItem, leftover = getNextData(leftover)
+		jobChans[threadId] <- nextItem
+
+		if nextItem == nil {
+			numOfJobs -= 1
+			close(jobChans[threadId])
+			logDebug("A thread has completed its job, %v remaining active thread(s)", numOfJobs)
+		}
+	}
+
+	close(idChan)
+	return itemsToUpdate
+}
+
+func getNextData(itemsToWorkOn []*ItemData) (*ItemData, []*ItemData) {
+	if len(itemsToWorkOn) > 0 {
+		return itemsToWorkOn[0], itemsToWorkOn[1:]
+	}
+
+	return nil, itemsToWorkOn
+}
+
+func getAndUpdateDataForItemThread(currentItem *ItemData, id int, jobChan chan *ItemData, idChan chan int) {
+	for currentItem != nil {
+		getAndUpdateData(currentItem)
+		idChan <- id
+		currentItem = <-jobChan
+		logDebug("Received job: %v", currentItem)
+	}
+
+	logDebug("Closing the channel")
+}
+
 func getAndUpdateData(item *ItemData) {
-	logDebug(`Processing "%v"`, item.Name)
+	logInfo(`Processing price update for %v`, item.Name)
 
 	for _, siteData := range item.SiteData {
 		var content *interface{}
@@ -30,10 +79,27 @@ func getAndUpdateData(item *ItemData) {
 		if price > 0 {
 			if history, ok := item.SiteHistory[siteData.Name]; ok {
 				history.AddPriceToLast24HourHistory(price)
-				logDebug("History: %q", history)
+				logInfo("Found the cost of %v from %v for %v", price, siteData.Name, item.Name)
 			}
 		}
 	}
 
-	logDebug(`Finished processing "%v"\n%q`, item.Name, item)
+	logInfo("Finished processing %v\n%q", item.Name, item)
+}
+
+func startSingleThread(item *ItemData, id int, idChan chan int) chan *ItemData {
+	jobChan := make(chan *ItemData)
+	go getAndUpdateDataForItemThread(item, id, jobChan, idChan)
+	return jobChan
+}
+
+func startThreads(allItems []*ItemData, numOfThreads int, idChan chan int) []chan *ItemData {
+	length := len(allItems)
+	var idChans []chan *ItemData
+
+	for threadId := 0; threadId < length && threadId < numOfThreads; threadId++ {
+		idChans = append(idChans, startSingleThread(allItems[threadId], threadId, idChan))
+	}
+
+	return idChans
 }
--- getter_test.go
+++ getter_test.go
@@ -11,6 +11,157 @@ var siteToUrl map[string]string
 
 func Test_getAndUpdateData_basicHtmlCase(t *testing.T) {
 	setup_server()
+	itemData := makeBasicItemDataForHtmlParsing()
+	getAndUpdateData(&itemData)
+
+	AssertEqual(itemData.SiteHistory["htmlHandler1"].GetLatestPrice(), 150.00, t)
+}
+
+func Test_getAndUpdateData_basicJsonCase(t *testing.T) {
+	setup_server()
+	itemData := makeBasicItemDataForJsonParsing()
+	getAndUpdateData(&itemData)
+
+	AssertEqual(itemData.SiteHistory["jsonHandler1"].GetLatestPrice(), 123.45, t)
+}
+
+func Test_getAndUpdateData_hasMultipleSources(t *testing.T) {
+	setup_server()
+	itemData := makeBasicItemDataForMultipleParsing()
+	getAndUpdateData(&itemData)
+
+	AssertEqual(itemData.SiteHistory["htmlHandler1"].GetLatestPrice(), 150.00, t)
+	AssertEqual(itemData.SiteHistory["htmlHandler2"].GetLatestPrice(), -1.0, t)
+	AssertEqual(itemData.SiteHistory["jsonHandler1"].GetLatestPrice(), 123.45, t)
+	AssertEqual(itemData.SiteHistory["jsonHandler2"].GetLatestPrice(), -1.0, t)
+}
+
+func Test_GetAndUpdateDataForItems_With1Goroutines(t *testing.T) {
+	setup_server()
+
+	items := []ItemData{makeBasicItemDataForHtmlParsing(), makeBasicItemDataForJsonParsing(), makeBasicItemDataForMultipleParsing()}
+	itemsToUpdate := []*ItemData{}
+
+	for _, item := range items {
+		itemsToUpdate = append(itemsToUpdate, &item)
+	}
+
+	GetAndUpdateDataForItems(itemsToUpdate, 1)
+
+	AssertEqual(itemsToUpdate[0].SiteHistory["htmlHandler1"].GetLatestPrice(), 150.00, t)
+	AssertEqual(itemsToUpdate[1].SiteHistory["jsonHandler1"].GetLatestPrice(), 123.45, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["htmlHandler1"].GetLatestPrice(), 150.00, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["htmlHandler2"].GetLatestPrice(), -1.0, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["jsonHandler1"].GetLatestPrice(), 123.45, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["jsonHandler2"].GetLatestPrice(), -1.0, t)
+}
+
+func Test_GetAndUpdateDataForItems_With2Goroutines(t *testing.T) {
+	setup_server()
+
+	items := []ItemData{makeBasicItemDataForHtmlParsing(), makeBasicItemDataForJsonParsing(), makeBasicItemDataForMultipleParsing()}
+	itemsToUpdate := []*ItemData{}
+
+	for _, item := range items {
+		itemsToUpdate = append(itemsToUpdate, &item)
+	}
+
+	GetAndUpdateDataForItems(itemsToUpdate, 2)
+
+	AssertEqual(itemsToUpdate[0].SiteHistory["htmlHandler1"].GetLatestPrice(), 150.00, t)
+	AssertEqual(itemsToUpdate[1].SiteHistory["jsonHandler1"].GetLatestPrice(), 123.45, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["htmlHandler1"].GetLatestPrice(), 150.00, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["htmlHandler2"].GetLatestPrice(), -1.0, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["jsonHandler1"].GetLatestPrice(), 123.45, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["jsonHandler2"].GetLatestPrice(), -1.0, t)
+}
+
+func Test_GetAndUpdateDataForItems_With3Goroutines(t *testing.T) {
+	setup_server()
+
+	items := []ItemData{makeBasicItemDataForHtmlParsing(), makeBasicItemDataForJsonParsing(), makeBasicItemDataForMultipleParsing()}
+	itemsToUpdate := []*ItemData{}
+
+	for _, item := range items {
+		itemsToUpdate = append(itemsToUpdate, &item)
+	}
+
+	GetAndUpdateDataForItems(itemsToUpdate, 3)
+
+	AssertEqual(itemsToUpdate[0].SiteHistory["htmlHandler1"].GetLatestPrice(), 150.00, t)
+	AssertEqual(itemsToUpdate[1].SiteHistory["jsonHandler1"].GetLatestPrice(), 123.45, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["htmlHandler1"].GetLatestPrice(), 150.00, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["htmlHandler2"].GetLatestPrice(), -1.0, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["jsonHandler1"].GetLatestPrice(), 123.45, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["jsonHandler2"].GetLatestPrice(), -1.0, t)
+}
+
+func Test_GetAndUpdateDataForItems_With4Goroutines(t *testing.T) {
+	setup_server()
+
+	items := []ItemData{makeBasicItemDataForHtmlParsing(), makeBasicItemDataForJsonParsing(), makeBasicItemDataForMultipleParsing()}
+	itemsToUpdate := []*ItemData{}
+
+	for _, item := range items {
+		itemsToUpdate = append(itemsToUpdate, &item)
+	}
+
+	GetAndUpdateDataForItems(itemsToUpdate, 4)
+
+	AssertEqual(itemsToUpdate[0].SiteHistory["htmlHandler1"].GetLatestPrice(), 150.00, t)
+	AssertEqual(itemsToUpdate[1].SiteHistory["jsonHandler1"].GetLatestPrice(), 123.45, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["htmlHandler1"].GetLatestPrice(), 150.00, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["htmlHandler2"].GetLatestPrice(), -1.0, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["jsonHandler1"].GetLatestPrice(), 123.45, t)
+	AssertEqual(itemsToUpdate[2].SiteHistory["jsonHandler2"].GetLatestPrice(), -1.0, t)
+}
+
+func setup_server() {
+	siteToUrl = make(map[string]string)
+	s := httptest.NewServer(http.HandlerFunc(htmlHandler1))
+	siteToUrl["htmlHandler1"] = s.URL
+
+	s = httptest.NewServer(http.HandlerFunc(jsonHandler1))
+	siteToUrl["jsonHandler1"] = s.URL
+
+	s = httptest.NewServer(http.HandlerFunc(htmlHandler2))
+	siteToUrl["htmlHandler2"] = s.URL
+
+	s = httptest.NewServer(http.HandlerFunc(jsonHandler2))
+	siteToUrl["jsonHandler2"] = s.URL
+
+	s = httptest.NewServer(http.HandlerFunc(htmlHandler3))
+	siteToUrl["htmlHandler3"] = s.URL
+
+	s = httptest.NewServer(http.HandlerFunc(jsonHandler3))
+	siteToUrl["jsonHandler3"] = s.URL
+}
+
+func htmlHandler1(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(rw, `<html><head></head><body><span class="check">150.00</span></body></html>`)
+}
+
+func jsonHandler1(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(rw, `{"item": "box", "price": 123.45}`)
+}
+
+func htmlHandler2(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(rw, `<html><head></head><body><span class="check">101.00</span></body></html>`)
+}
+
+func jsonHandler2(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(rw, `{"item": "box", "price": 13.00}`)
+}
+
+func htmlHandler3(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(rw, `<html><head></head><body><span class="check">105.00</span></body></html>`)
+}
+
+func jsonHandler3(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(rw, `{"item": "box", "price": 45.00}`)
+}
+
+func makeBasicItemDataForHtmlParsing() ItemData {
 	itemSiteData := make([]ItemSiteData, 0)
 	itemSiteData = append(itemSiteData, ItemSiteData{
 		SiteId:     1,
@@ -26,13 +177,11 @@ func Test_getAndUpdateData_basicHtmlCase(t *testing.T) {
 		SiteHistory: make(map[string]*SiteHistory)}
 
 	itemData.SiteHistory["htmlHandler1"] = &SiteHistory{}
-	getAndUpdateData(&itemData)
 
-	AssertEqual(itemData.SiteHistory["htmlHandler1"].GetLatestPrice(), 150.00, t)
+	return itemData
 }
 
-func Test_getAndUpdateData_basicJsonCase(t *testing.T) {
-	setup_server()
+func makeBasicItemDataForJsonParsing() ItemData {
 	itemSiteData := make([]ItemSiteData, 0)
 	itemSiteData = append(itemSiteData, ItemSiteData{
 		SiteId:     1,
@@ -48,24 +197,50 @@ func Test_getAndUpdateData_basicJsonCase(t *testing.T) {
 		SiteHistory: make(map[string]*SiteHistory)}
 
 	itemData.SiteHistory["jsonHandler1"] = &SiteHistory{}
-	getAndUpdateData(&itemData)
 
-	AssertEqual(itemData.SiteHistory["jsonHandler1"].GetLatestPrice(), 123.00, t)
+	return itemData
 }
 
-func setup_server() {
-	siteToUrl = make(map[string]string)
-	s := httptest.NewServer(http.HandlerFunc(htmlHandler1))
-	siteToUrl["htmlHandler1"] = s.URL
+func makeBasicItemDataForMultipleParsing() ItemData {
+	itemSiteData := make([]ItemSiteData, 0)
+	itemSiteData = append(itemSiteData, ItemSiteData{
+		SiteId:     1,
+		Name:       "htmlHandler1",
+		ParserType: htmlParser,
+		Path:       []interface{}{".check"},
+		Url:        siteToUrl["htmlHandler1"]})
 
-	s = httptest.NewServer(http.HandlerFunc(jsonHandler1))
-	siteToUrl["jsonHandler1"] = s.URL
-}
+	itemSiteData = append(itemSiteData, ItemSiteData{
+		SiteId:     2,
+		Name:       "htmlHandler2",
+		ParserType: htmlParser,
+		Path:       []interface{}{".check1"},
+		Url:        siteToUrl["htmlHandler2"]})
 
-func htmlHandler1(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(rw, `<html><head></head><body><span class="check">150.00</span></body></html>`)
-}
+	itemSiteData = append(itemSiteData, ItemSiteData{
+		SiteId:     3,
+		Name:       "jsonHandler1",
+		ParserType: jsonParser,
+		Path:       []interface{}{"price"},
+		Url:        siteToUrl["jsonHandler1"]})
 
-func jsonHandler1(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(rw, `{"item": "box", "price": 123.00}`)
+	itemSiteData = append(itemSiteData, ItemSiteData{
+		SiteId:     4,
+		Name:       "jsonHandler2",
+		ParserType: jsonParser,
+		Path:       []interface{}{".check"},
+		Url:        siteToUrl["jsonHandler2"]})
+
+	itemData := ItemData{
+		Name:        "Box",
+		ItemId:      1,
+		SiteData:    itemSiteData,
+		SiteHistory: make(map[string]*SiteHistory)}
+
+	itemData.SiteHistory["htmlHandler1"] = &SiteHistory{}
+	itemData.SiteHistory["htmlHandler2"] = &SiteHistory{}
+	itemData.SiteHistory["jsonHandler1"] = &SiteHistory{}
+	itemData.SiteHistory["jsonHandler2"] = &SiteHistory{}
+
+	return itemData
 }
